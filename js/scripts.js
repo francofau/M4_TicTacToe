17121b3d54bc0b96aa277b68bcd542a261d99794
Aziz863 <55482239+Aziz863@users.noreply.github.com>
2021-06-04 20:57:51 +0200

    final sesión 1

--- js/scripts.js
+++ js/scripts.js
@@ -16,42 +16,134 @@ const opcionVictoria = [
 
 let jugador1 = 'Aziz'
 let jugador2 = 'Franco'
-
 let mensaje = $('#mensaje');
 
+let fichasJugador1 = [];
+let fichasJugador2 = [];
+
+let indexJugador1 = 0;
+let indexJugador2 = 0;
+
+
 //función del click de cada celda
-function controlClick(valor) {
+controlClick = (valor) => {
 
-
-
+    //sacamos el número de la celda
     let id = valor.getAttribute('id');
     id = id.split('-')[1];
-    console.log(turno);
-    turno = pintarCelda(id, turno);
+
+
+
+    //si hay 3 fichas del jugador borra una de ellas (pasamos a tener 2)
+    if (fichas(turno) == 3) {
+        if (turno == $('#celda-' + id).html()) {
+            borrarPosicion(id);
+        }
+
+        //si se tiene menos de 3 fichas
+    } else {
+        //si la celda esta vacía
+        if (valor.textContent == '') {
+            rellenarArrayTablero(turno, id) //marcamos la posición en el array del tablero
+            pintarCelda(id, turno); //pintamos la ficha del jugador en la celda seleccionada
+            turno = controlTurno(turno); //cambiamos el turno
+        }
+
+        //si la casilla esta ocupada, mandamos mensaje de error
+        else mensaje.html('Error, casilla no válida');
+    }
 
 }
 
-//función para pintar
-function pintarCelda(objetivo, posicion) {
+//función para pintar la ficha en la celda
+pintarCelda = (objetivo, posicion) => {
     $('#celda-' + objetivo).html(posicion);
+}
 
-    if (turno == 'X') {
-        mensaje.html('Turno de ' + jugador1)
-        turno = 'O';
-    } else if (turno == 'O') {
-        turno = 'X';
+//control de turno
+controlTurno = (val) => {
+    if (val == 'X') {
         mensaje.html('Turno de ' + jugador2)
+        val = 'O';
+        return val;
+    } else if (val == 'O') {
+        val = 'X';
+        mensaje.html('Turno de ' + jugador1)
+        return val;
     }
-
-    return turno;
 }
 
 //input de los nombres de los jugadores
 
-//función enviar el contenido al array del tablero
+//función para enviar el contenido al array del tablero
+rellenarArrayTablero = (jugador, posicion) => {
+    tablero[posicion] = jugador;
+    comprobar(turno);
+}
 
 //comprobar opcionVictoria
+comprobar = (turno) => {
+    indexJugador1 = 0;
+    indexJugador2 = 0;
+    let coincidencias = 0;
+    //array que recorre el tablero y guarda las pocisicones de cada jugador
+    for (var i = 0; i < tablero.length; ++i) {
+        if (tablero[i] == turno) {
+
+            if (tablero[i] == 'X') {
+                fichasJugador1[indexJugador1] = i;
+                indexJugador1++
+                console.log(' fichas 1: ' + fichasJugador1);
+            }
+
+            if (tablero[i] == 'O') {
+                fichasJugador2[indexJugador2] = i;
+                indexJugador2++
+                console.log('fichas 2: ' + fichasJugador2);
+            }
+        }
+    }
+
+    //array que comprueba las opciones de victoria de cada jugador
+
+}
+
 
 //comprobar número fichas
+fichas = (turno) => {
+    let rep = 0;
 
-//funcción cuando hay 3 fichas
+    for (var i = 0; i < tablero.length; ++i) {
+        if (tablero[i] == turno) {
+            rep++;
+        }
+
+    }
+
+    return rep;
+}
+
+//borrar posición
+borrarPosicion = (posicion) => {
+    tablero[posicion] = '';
+    $('#celda-' + posicion).html('');
+}
+
+
+
+
+
+
+//prueba
+for (let index = 0; index < opcionVictoria.length; index++) {
+
+    for (let index2 = 0; index2 < 3; index2++) {
+        for (let index3 = 0; index3 < fichasJugador1.length; index3++) {
+            if (fichasJugador1 == opcionVictoria[index]) {
+                console.log('Victoria');
+            }
+
+        }
+
+    }
+}
